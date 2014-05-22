281dab248332594be109f338f4f0346cc556bc9a
Kaleb Himes <kaleb@wolfssl.com>
2014-05-22 10:51:49 -0600

    updated coding standards

--- project1/server-udp.c
+++ project1/server-udp.c
@@ -1,3 +1,28 @@
+/* server-udp.c 
+ *
+ * Copyright (C) 2006-2014 wolfSSL Inc.
+ *
+ * This file is part of CyaSSL.
+ *
+ * CyaSSL is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * CyaSSL is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,  
+ * USA
+ *=============================================================================
+ *
+ * Bare-bones example of a UDP server for instructional/learning purposes.
+ */
+
 #include <stdio.h>                      /* standard in/out procedures */
 #include <stdlib.h>	                    /* defines system calls */
 #include <string.h>	                    /* necessary for memset */
