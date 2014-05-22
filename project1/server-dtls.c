da8a38c8af24634c1148a92034ca2cc12c0036a5
Kaleb Himes <kaleb@wolfssl.com>
2014-05-22 14:32:59 -0600

    fixed valgrind error with DTLS v1.2 and AES

--- project1/server-dtls.c
+++ project1/server-dtls.c
@@ -18,28 +18,29 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,  
  * USA
- * =============================================================================
+ *=============================================================================
  *
  * Bare-bones example of a DTLS server for instructional/learning purposes.
  * Utilizes DTLS 1.2.
  */
 
-#include <stdio.h>          /* standard in/out procedures */
-#include <stdlib.h>         /* defines system calls */
-#include <string.h>         /* necessary for memset */
+#include <stdio.h>                  /* standard in/out procedures */
+#include <stdlib.h>                 /* defines system calls */
+#include <string.h>                 /* necessary for memset */
 #include <netdb.h>
-#include <sys/socket.h>     /* used for all socket calls */
-#include <netinet/in.h>     /* used for sockaddr_in */
+#include <sys/socket.h>             /* used for all socket calls */
+#include <netinet/in.h>             /* used for sockaddr_in */
 #include <arpa/inet.h>
 #include <cyassl/ssl.h>
 #include <errno.h>
 #include <signal.h>
 #include <unistd.h>
 
-#define SERV_PORT   11111               /* define our server port number */
+#define SERV_PORT   11111           /* define our server port number */
 #define MSGLEN      4096
 
-static int cleanup;                     /* To handle shutdown */
+static int cleanup;                 /* To handle shutdown */
+
 
 void sig_handler(const int sig) 
 {
@@ -48,6 +49,7 @@ void sig_handler(const int sig)
     exit(0);
 }
 
+
 static void err_sys(const char* msg)
 {
     printf("error: %s\n", msg);
@@ -55,24 +57,26 @@ static void err_sys(const char* msg)
         exit(EXIT_FAILURE);
 }
 
+
 int main(int argc, char** argv)
 {
     /* CREATE THE SOCKET */
 
     char ack[] = "I hear you fashizzle!";
+    struct sockaddr_in servaddr;    /* our server's address */
+    struct sockaddr_in cliaddr;     /* the client's address */
+    int listenfd = 0;               /* Initialize our socket */
+    socklen_t clilen;               /* length of address' */
+    int recvlen = 0;                /* length of message */
+    char buff[MSGLEN];              /* the incoming message */
+    struct sigaction    act, oact;  /* structures for signal handling */
 
-    struct sockaddr_in servaddr;        /* our server's address */
-    struct sockaddr_in cliaddr;         /* the client's address */
-    int listenfd;                       /* Initialize our socket */
-    socklen_t clilen;                   /* length of address' */
-    int recvlen;                        /* length of message */
-    char buff[MSGLEN];                  /* the incoming message */
-    struct sigaction    act, oact;      /* structures for signal handling */
 
-    /* Define a signal handler for when the user closes the program
-     *        with Ctrl-C. Also, turn off SA_RESTART so that the OS doesn't 
-     *               restart the call to accept() after the signal is handled. */
-
+    /* 
+     * Define a signal handler for when the user closes the program
+     * with Ctrl-C. Also, turn off SA_RESTART so that the OS doesn't 
+     * restart the call to accept() after the signal is handled. 
+     */
     act.sa_handler = sig_handler;
     sigemptyset(&act.sa_mask);
     act.sa_flags = 0;
@@ -112,53 +116,65 @@ int main(int argc, char** argv)
     }
     printf("Loaded server keys\n");
 
-    if ( (listenfd = socket(AF_INET, SOCK_DGRAM, 0) ) < 0 ) {
-        err_sys("cannot create socket");
-        return 0;
-    }
-    printf("Socket allocated\n");
-
-    /* INADDR_ANY=IP address and socket =  11111, modify SERV_PORT to change */
-    memset((char *)&servaddr, 0, sizeof(servaddr));
-
-    /* host-to-network-long conversion (htonl) */
-    /* host-to-network-short conversion (htons) */
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(SERV_PORT);
 
 
+    /* BEGIN: avoid valgrind error when using DTLS v1.2 and AES */
+    int ret;
 
-    /* Eliminate socket already in use error */
-    int res, on = 1;
-    socklen_t len = sizeof(on);
-    res = setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &on, len);
-    if (res < 0) {
-        err_sys("setsockopt SO_REUSEADDR failed\n");
-    }
-
-
-    /*Bind Socket*/
-    if (bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
-        err_sys("bind failed");
-        return 0;
-    }
-    printf("Socket bind complete\n");
-
+    ret = CyaSSL_CTX_set_cipher_list(ctx, "DES-CBC3-SHA");
+    if(ret == SSL_SUCCESS)
+        printf("ret successfully set to \"DES-CBC3-SHA\"\n");
+    /* END: avoid valgrind error */
 
 
 
 
     while (cleanup != 1) {
 
+        if ( (listenfd = socket(AF_INET, SOCK_DGRAM, 0) ) < 0 ) {
+            err_sys("cannot create socket");
+            return 0;
+        }
+        printf("Socket allocated\n");
+
+        /* INADDR_ANY=IPaddr, socket =  11111, modify SERV_PORT to change */
+        memset((char *)&servaddr, 0, sizeof(servaddr));
+
+        /* host-to-network-long conversion (htonl) */
+        /* host-to-network-short conversion (htons) */
+        servaddr.sin_family = AF_INET;
+        servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+        servaddr.sin_port = htons(SERV_PORT);
+
+
+
+        /* Eliminate socket already in use error */
+        int res = 1; 
+        int on = 1;
+        socklen_t len = sizeof(on);
+        res = setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &on, len);
+        if (res < 0) {
+            err_sys("setsockopt SO_REUSEADDR failed\n");
+        }
+
+
+        /*Bind Socket*/
+        if (bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+            err_sys("bind failed");
+            return 0;
+        }
+        printf("Socket bind complete\n");
+
+
         printf("Awaiting client connection on port %d\n", SERV_PORT);
 
-        CYASSL*             ssl = 0;    /* initialize arg */
+        CYASSL*                 ssl;    /* initialize arg */
         clilen =    sizeof(cliaddr);    /* set clilen to |cliaddr| */
         unsigned char       b[1500];    
-        int                  connfd;     
+        int              connfd = 0;     
 
-        connfd = (int)recvfrom(listenfd, (char*)b, sizeof(b), MSG_PEEK,
+
+        connfd = (int)recvfrom(listenfd, (char *)&b, sizeof(b), MSG_OOB,
                 (struct sockaddr*)&cliaddr, &clilen);
 
         if (connfd < 0){
@@ -183,17 +199,23 @@ int main(int argc, char** argv)
             exit(EXIT_FAILURE);
         }
 
+/******/printf("STEP 1\n");
+
+
         /* set the session ssl to client connection port */
         CyaSSL_set_fd(ssl, listenfd);
 
+/******/printf("STEP 2\n");
 
         if (CyaSSL_accept(ssl) != SSL_SUCCESS) {
             int err = CyaSSL_get_error(ssl, 0);
             char buffer[80];
             printf("error = %d, %s\n", err, CyaSSL_ERR_error_string(err, buffer));
+            buffer[80]= 0;
             err_sys("SSL_accept failed\n");
         }
 
+/*******/printf("STEP 3\n");
 
         if (( recvlen = CyaSSL_read(ssl, buff, sizeof(buff)-1)) > 0){
 
@@ -203,13 +225,17 @@ int main(int argc, char** argv)
             printf("I heard this: \"%s\"\n", buff);
         }
 
+/*******/printf("STEP 4\n");
+
         if (recvlen < 0) {
             int readErr = CyaSSL_get_error(ssl, 0);
             if(readErr != SSL_ERROR_WANT_READ)
                 err_sys("SSL_read failed");
         }
 
-        if (CyaSSL_write(ssl, ack, sizeof(ack)-1) < 0) {
+/******/printf("STEP 5\n");
+
+        if (CyaSSL_write(ssl, ack, sizeof(ack)) < 0) {
             err_sys("CyaSSL_write fail");
         }
 
@@ -221,12 +247,11 @@ int main(int argc, char** argv)
 
         CyaSSL_set_fd(ssl, 0); 
         CyaSSL_shutdown(ssl);        
-        //        CyaSSL_free(ssl);
-        //
+        CyaSSL_free(ssl);
 
         printf("Client left return to idle state\n");
         continue;
     }
-    //    CyaSSL_CTX_free(ctx);
+    CyaSSL_CTX_free(ctx);
     return(0);
 }
