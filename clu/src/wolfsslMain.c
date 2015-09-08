83e261621a0a9591c1dd724ce3d93e0591854eb6
kaleb-himes <kaleb@wolfssl.com>
2015-09-08 14:42:22 -0600

    switch default case to default help

--- clu/src/wolfsslMain.c
+++ clu/src/wolfsslMain.c
@@ -82,7 +82,7 @@ int main(int argc, char** argv)
                             return 0;
              default:
                             printf("Main help default.\n");
-                            wolfsslVerboseHelp();
+                            wolfsslHelp();
                             return 0;
         }
     }
