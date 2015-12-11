6d9d39ef63e43be78511ec416605ec0513ef12d1
kaleb-himes <kaleb@wolfssl.com>
2015-12-11 11:17:24 -0800

    Development is moving to https://github.com/wolfSSL/wolfCLU

--- clu/tests/somejunk/Readme.md
+++
@@ -1,4 +0,0 @@
-run whichever .sh you want to generate a plain text file containing the number
-of lines specified in the .sh filename. Test encrypting and decrypting on these
-ascii based files.
-
--- clu/tests/somejunk/somejunk_100.sh
+++
@@ -1,9 +0,0 @@
-for i in {1..100}
-do
-    if [ $i -eq 1 ]
-    then
-        echo "This is a file that contains 100 lines of text for encryption testing" > somejunk_100.txt
-    else
-        echo "This is a file that contains 100 lines of text for encryption testing" >> somejunk_100.txt
-    fi
-done
