d42a176965e0d24ce614b32ba6426b5bb77cc9f2
kaleb-himes <kaleb@wolfssl.com>
2016-11-24 08:49:17 -0700

    version update

--- wolfCLU/clu_include/version.h
+++ wolfCLU/clu_include/version.h
@@ -26,8 +26,8 @@
 extern "C" {
 #endif
 
-#define LIBWOLFSSL_VERSION_STRING "0.4"
-#define LIBWOLFSSL_VERSION_HEX 0x00004000
+#define LIBWOLFSSL_VERSION_STRING "0.5"
+#define LIBWOLFSSL_VERSION_HEX 0x00005000
 
 #ifdef __cplusplus
 }
