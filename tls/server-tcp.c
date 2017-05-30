716a64546c7c760272393af1cbfcdc65ab2d6cd2
Levi Rak <levi@wolfssl.com>
2017-05-30 15:21:55 -0600

    flattened server-tcp

--- tls/server-tcp.c
+++ tls/server-tcp.c
@@ -19,116 +19,116 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <string.h>
-#include <netinet/in.h>
+/* the usual suspects */
 #include <stdlib.h>
-#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+
+/* error reporting */
+#include <err.h>
+
+/* socket includes */
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+
 
 #define DEFAULT_PORT 11111
 
-int AcceptAndRead(socklen_t sockfd);
-
-int AcceptAndRead(socklen_t sockfd)
-{
-    struct sockaddr_in clientAddr;
- 	socklen_t size = sizeof(clientAddr);
-    int ret  = 0;
-
-    /* Wait until a client connects */
-    int connd = accept(sockfd,(struct sockaddr *)&clientAddr, &size);
-
-    /* If fails to connect, loop back up and wait for a new connection */
-    if (connd == -1){
-        printf("failed to accept the connection..\n");
-    }
-    /* If it connects, read in and reply to the client */
-    else{
-
-        printf("Client connected successfully\n");
-
-        for ( ; ; ){
-
-            char buff[256];
-
-            /* Clear the buffer memory for anything  possibly left over */
-            bzero(&buff, sizeof(buff));
-            
-            /* Read the client data into our buff array */
-            if ((ret = read(connd, buff, sizeof(buff)-1)) > 0){
-                /* Print any data the client sends to the console */
-                printf("Client: %s\n", buff);
-
-                /* Create our reply message */
-                char reply[] = "I hear ya fa shizzle!\n";
-                
-                /* Reply back to the client */
-                if ((ret = write(connd, reply, sizeof(reply)-1)) < 0)
-                    printf("write error\n");
-            }
-            /* If the client disconnects break the loop */
-            else
-                break;
-        }
-    }
-
-    /* Close the socket */
-    close(connd);
-    
-    return 0;
-}
-
-
 int main()
 {
-    /* 
-     * Creates a socket that uses an internet IP address,
-     * Sets the type to be Stream based (TCP),
+    int                sockfd;
+    int                connd;
+    char               buff[256];
+    struct sockaddr_in serverAddr;
+    struct sockaddr_in clientAddr;
+    socklen_t          size = sizeof(clientAddr);
+    int                ret = 0;
+
+
+    /*
+     * Creates a socket that uses an internet IPv4 address,
+     * Sets the socket to be stream based (TCP),
      * 0 means choose the default protocol.
      */
-
-     /* Identify and access the sockets */
-    socklen_t sockfd = socket(AF_INET, SOCK_STREAM, 0); 
-    int exit   = 0; 	/* 0 = false, 1 = true */
-
-    /* If positive value, the socket is valid */
-    if(sockfd == -1){
-        printf("ERROR: failed to create the socket\n");
-        return 1;        /* Kill the server with exit status 1 */        
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        /* exit the program with -1 after printing the message to stderr */
+        errx(-1, "failed to create the socket");
     }
 
-    /* Server and client socket address structures */
-	struct sockaddr_in serverAddr;
 
-	/* Initialize the server address struct to zero */
-    memset((char *)&serverAddr, 0, sizeof(serverAddr)); 
+    /* Initialize the server address struct to zero */
+    memset(&serverAddr, 0, sizeof(serverAddr));
 
     /* Fill the server's address family */
-    serverAddr.sin_family	   = AF_INET;
+    serverAddr.sin_family      = AF_INET;
     serverAddr.sin_addr.s_addr = INADDR_ANY;
     serverAddr.sin_port        = htons(DEFAULT_PORT);
 
+
     /* Attach the server socket to our port */
-    if(bind(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0){
-        printf("ERROR: failed to bind\n");
-        return 1;
+    if (bind(sockfd, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
+        errx(-1, "failed to bind");
     }
 
-    /* Continuously accept connections while not currently in an active connection or told to quit */
-    while (exit == 0){
-        /* Listen for a new connection, allow 5 pending connections */
-        listen(sockfd, 5);
-        printf("Waiting for a connection...\n");
 
-        /* Accept client connections and read from them */
-    	exit = AcceptAndRead(sockfd);
+    /* Listen for a new connection, allow 5 pending connections */
+    if (listen(sockfd, 5) == -1) {
+        close(sockfd);
+        errx(-1, "failed to listen");
     }
 
-    /* Close the open sockets */
-    close(sockfd);
-    return 0;
+
+    printf("Waiting for a connection...\n");
+
+    /* Accept client connections */
+    if ((connd = accept(sockfd, (struct sockaddr*)&clientAddr, &size)) == -1) {
+        close(sockfd);
+        errx(-1, "failed to accept the connection\n");
+    }
+
+
+    printf("Client connected successfully\n");
+
+    /* Read in from the client while there is something to read */
+    do {
+        /* Clear the buffer memory for anything  possibly left over */
+        memset(buff, 0, sizeof(buff));
+
+
+        /* Read the client data into our buff array */
+        if ((ret = read(connd, buff, sizeof(buff)-1)) > 0) {
+            /* Print any data the client sends to the console */
+            printf("Client: %s\n", buff);
+
+
+            /* Write our reply into buff */
+            memset(buff, 0, sizeof(buff));
+            memcpy(buff, "I hear ya fa shizzle!\n", 23);
+
+
+            /* Reply back to the client */
+            if ((ret = write(connd, buff, sizeof(buff)-1)) < 0) {
+                /* Write an error without exiting the program */
+                warnx("write error");
+            }
+        }
+    } while (ret > 0);
+
+
+    /* Check for a read error condition */
+    if (ret < 0) {
+        warnx("read error");
+    }
+    else {
+        printf("Client has closed the connection.\n");
+    }
+
+
+    /* Cleanup and return */
+    close(connd);  /* Close the connection to the client     */
+    close(sockfd); /* Close the socket listening for clients */
+    return 0;      /* Return reporting a success             */
 
 }
