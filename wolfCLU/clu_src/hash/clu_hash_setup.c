aa581dd18a4a37aa357ec414fda2d15ed216ff1b
Levi Rak <levi@wolfssl.com>
2017-08-17 12:56:11 -0600

    make hash use wolfCLU_checkForArgs()

--- wolfCLU/clu_src/hash/clu_hash_setup.c
+++ wolfCLU/clu_src/hash/clu_hash_setup.c
@@ -32,30 +32,32 @@ int wolfCLU_hashSetup(int argc, char** argv)
     char*   out     =   NULL;   /* output variable */
     const char* algs[]  =   {   /* list of acceptable algorithms */
 #ifndef NO_MD5
-        "md5"
+        "md5",
 #endif
 #ifndef NO_SHA
-        , "sha"
+        "sha",
 #endif
 #ifndef NO_SHA256
-        , "sha256"
+        "sha256",
 #endif
 #ifdef WOLFSSL_SHA384
-        , "sha384"
+        "sha384",
 #endif
 #ifdef WOLFSSL_SHA512
-        , "sha512"
+        "sha512",
 #endif
 #ifdef HAVE_BLAKE2
-        , "blake2b"
+        "blake2b",
 #endif
 #ifndef NO_CODING
     #ifdef WOLFSSL_BASE64_ENCODE
-        , "base64enc"
+        "base64enc",
     #endif
-        , "base64dec"
+        "base64dec",
 #endif
+        NULL /* terminal element (also stops the array from being 0-size */
     };
+    size_t algsSz = sizeof(algs) / sizeof(algs[0]) - 1; /* -1 to ignore NULL */
 
     char*   alg;                /* algorithm being used */
     int     algCheck=   0;      /* acceptable algorithm check */
@@ -67,15 +69,13 @@ int wolfCLU_hashSetup(int argc, char** argv)
 #endif
 
     /* help checking */
-    for (i = 2; i < argc; i++) {
-        if (XSTRNCMP(argv[i], "-help", 5) == 0 || XSTRNCMP(argv[i], "-h", 2)
-                                                                         == 0) {
-            wolfCLU_hashHelp();
-            return 0;
-        }
+    ret = wolfCLU_checkForArg("-help", 5, argc, argv);
+    if (ret > 0) {
+        wolfCLU_hashHelp();
+        return 0;
     }
 
-    for (i = 0; i < (int) sizeof(algs)/(int) sizeof(algs[0]); i++) {
+    for (i = 0; i < (int)algsSz; ++i) {
         /* checks for acceptable algorithms */
         if (XSTRNCMP(argv[2], algs[i], XSTRLEN(algs[i])) == 0) {
             alg = argv[2];
@@ -88,45 +88,43 @@ int wolfCLU_hashSetup(int argc, char** argv)
         return FATAL_ERROR;
     }
 
-    for (i = 3; i < argc; i++) {
-        if (XSTRNCMP(argv[i], "-in", 3) == 0 && argv[i+1] != NULL) {
-            /* input file/text */
-            in = XMALLOC(strlen(argv[i+1])+1, HEAP_HINT,
-                                                      DYNAMIC_TYPE_TMP_BUFFER);
-            if (in == NULL)
-                return MEMORY_E;
+    ret = wolfCLU_checkForArg("-in", 3, argc, argv);
+    if (ret > 0) {
+        /* input file/text */
+        in = XMALLOC(strlen(argv[i+1])+1, HEAP_HINT, DYNAMIC_TYPE_TMP_BUFFER);
+        if (in == NULL)
+            return MEMORY_E;
 
-            XSTRNCPY(in, &argv[i+1][0], XSTRLEN(&argv[i+1][0]));
-            in[XSTRLEN(argv[i+1])] = '\0';
-            inCheck = 1;
-            i++;
-        }
-        else if (XSTRNCMP(argv[i], "-out", 4) == 0 && argv[i+1] != NULL) {
-            /* output file */
-            out = argv[i+1];
-            i++;
-        }
-        else if (XSTRNCMP(argv[i], "-size", 5) == 0 && argv[i+1] != NULL) {
-            /* size of output */
-#ifndef HAVE_BLAKE2
-            printf("Sorry, only to be used with Blake2b enabled\n");
-#else
-            size = atoi(argv[i+1]);
-            if (size <= 0 || size > 64) {
-                printf("Invalid size, Must be between 1-64. Using default.\n");
-                size = BLAKE_DIGEST_SIZE;
-            }
-#endif
-            i++;
-        }
-        else {
-            printf("Unknown argument %s. Ignoring\n", argv[i]);
-        }
+        XSTRNCPY(in, &argv[ret+1][0], XSTRLEN(argv[ret+1]));
+        in[XSTRLEN(argv[ret+1])] = '\0';
+        inCheck = 1;
     }
+
+    ret = wolfCLU_checkForArg("-out", 4, argc, argv);
+    if (ret > 0) {
+        /* output file */
+        out = argv[ret+1];
+    }
+
+    ret = wolfCLU_checkForArg("-size", 5, argc, argv);
+    if (ret > 0) {
+        /* size of output */
+#ifndef HAVE_BLAKE2
+        printf("%s: -size is only valid when blake2 in enabled.\n", argv[0]);
+#else
+        size = atoi(argv[ret+1]);
+        if (size <= 0 || size > 64) {
+            printf("Invalid size, Must be between 1-64. Using default.\n");
+            size = BLAKE_DIGEST_SIZE;
+        }
+#endif
+    }
+
     if (inCheck == 0) {
         printf("Must have input as either a file or standard I/O\n");
         return FATAL_ERROR;
     }
+
     /* sets default size of algorithm */
 #ifndef NO_MD5
     if (XSTRNCMP(alg, "md5", 3) == 0)
