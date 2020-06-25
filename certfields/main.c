ee78caaeec4d9a4bb35acd1539dd19f2fbae2e27
kaleb-himes <kaleb@wolfssl.com>
2020-06-25 14:24:01 -0600

    Extend certfields to output alt names and tie in w/ certgen example

--- certfields/main.c
+++ certfields/main.c
@@ -31,6 +31,11 @@
 #include <wolfssl/wolfcrypt/rsa.h>
 #include <wolfssl/wolfcrypt/asn.h>
 
+enum {
+    RSA_KEY_TYPE = 2,
+    ECC_KEY_TYPE = 3,
+};
+
 static void err_sys(const char* msg, int ret)
 {
     if (ret) {
@@ -41,7 +46,7 @@ static void err_sys(const char* msg, int ret)
     exit(EXIT_FAILURE);
 }
 
-int main(void)
+int main(int argc, char** argv)
 {
     int   ret, i;
     int   sigType;
@@ -50,8 +55,11 @@ int main(void)
     byte  derCert[4096];
     word32 idx;
     FILE* file;
+    char* certFile;
+    int keyType;
 
-    RsaKey pubKey;
+    RsaKey pubKeyRsa;
+    ecc_key pubKeyEcc;
     WOLFSSL_X509* cert;
     WOLFSSL_EVP_PKEY* pubKeyTmp;
     WOLFSSL_X509_NAME* name;
@@ -63,10 +71,27 @@ int main(void)
     char orgName[80];
     char orgUnit[80];
 
+    STACK_OF(GENERAL_NAME)* sk;
+
     /* ------ PARSE ORIGINAL SELF-SIGNED CERTIFICATE ------ */
 
+    if (argc < 3) {
+        printf("USAGE:\n"
+               "./app derCert keyType\n"
+               "EXAMPLE(s):\n"
+               "            ./app myCert.der RSA\n"
+               "            ./app myCert.der ECC\n");
+        err_sys("invalid input", -1);
+    }
+    certFile = argv[1]; /* certFile to check */
+    keyType = (XMEMCMP(argv[2], "RSA", 3) == 0) ? RSA_KEY_TYPE :
+                (XMEMCMP(argv[2], "ECC", 3) == 0) ? ECC_KEY_TYPE : 0; /* key */
+    if (keyType == 0)
+        err_sys("unsupported keyType", -1);
+
     /* open and read DER-formatted cert into buffer */
-    file = fopen("../certs/client-cert.der", "rb");
+    //file = fopen("../certs/client-cert.der", "rb");
+    file = fopen(certFile, "rb");
     if (!file)
         err_sys("can't open client certificate", 0);
 
@@ -85,10 +110,17 @@ int main(void)
     if (pubKeyTmp == NULL)
         err_sys("wolfSSL_X509_get_pubkey failed", 0);
 
-    wc_InitRsaKey(&pubKey, 0);
     idx = 0;
-    ret = wc_RsaPublicKeyDecode((byte*)pubKeyTmp->pkey.ptr, &idx, &pubKey,
-                                pubKeyTmp->pkey_sz);
+    if (keyType == RSA_KEY_TYPE) {
+        wc_InitRsaKey(&pubKeyRsa, 0);
+        ret = wc_RsaPublicKeyDecode((byte*)pubKeyTmp->pkey.ptr, &idx,
+                                    &pubKeyRsa, pubKeyTmp->pkey_sz);
+    } else {
+        wc_ecc_init(&pubKeyEcc);
+        ret = wc_EccPublicKeyDecode((byte*)pubKeyTmp->pkey.ptr, &idx,
+                                    &pubKeyEcc, pubKeyTmp->pkey_sz);
+    }
+
     if (ret != 0)
         err_sys("wc_RsaPublicKeyDecode failed", ret);
 
@@ -131,6 +163,27 @@ int main(void)
     nameSz = wolfSSL_X509_NAME_get_text_by_NID(name, ASN_ORGUNIT_NAME,
                                             orgUnit, sizeof(orgUnit));
     printf("ORG UNIT = %s (%d)\n", orgUnit, nameSz);
+
+    sk = X509_get_ext_d2i(cert, NID_subject_alt_name, NULL, NULL);
+
+    for (i = 0; i < sk_GENERAL_NAME_num(sk); i++) {
+        GENERAL_NAME* gn = sk_GENERAL_NAME_value(sk, i);
+        if (gn->type == GEN_DNS) {
+            printf("DNS:%s\n", gn->d.dNSName->strData);
+        } else if (gn->type == GEN_IPADD) {
+            printf("IP:");
+            int j;
+            for (j = 0; j < gn->d.ip->length; j++) {
+                if (j < gn->d.ip->length - 1)
+                    printf("%d.", (int) gn->d.ip->strData[j]);
+                else
+                    printf("%d", (int) gn->d.ip->strData[j]);
+            }
+            printf("\n");
+        } else {
+            printf("Other type: %d\n", gn->type);
+        }
+    }
 
     wolfSSL_EVP_PKEY_free(pubKeyTmp);
     wolfSSL_X509_free(cert);
