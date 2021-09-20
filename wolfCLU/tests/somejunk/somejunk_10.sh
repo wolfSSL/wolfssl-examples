da8f86333bc0ef909463d08aac46cc5d1f41211a
Jacob Barthelmeh <jacob@wolfssl.com>
2021-09-20 11:31:52 -0600

    wolfCLU was moved to its own repository

--- wolfCLU/tests/somejunk/somejunk_10.sh
+++
@@ -1,9 +0,0 @@
-for i in {1..10}
-do
-    if [ $i -eq 1 ]
-    then
-        echo "This is a file that contains 10 lines of text for encryption testing" > somejunk_10.txt
-    else
-        echo "This is a file that contains 10 lines of text for encryption testing" >> somejunk_10.txt
-    fi
-done
--- wolfCLU/tests/testEncDec/test_aesctr.sh
+++
@@ -1,9 +0,0 @@
-###### AES CTR ######
-wolfssl -encrypt aes-ctr-128 -pwd hello128 -in ../somejunk/somejunk_100000.txt -out enc_aes_ctr_128.txt
-wolfssl -decrypt aes-ctr-128 -in enc_aes_ctr_128.txt -out dec_aes_ctr_128.txt -pwd hello128
-
-wolfssl -encrypt aes-ctr-192 -pwd hello192 -in ../somejunk/somejunk_100000.txt -out enc_aes_ctr_192.txt
-wolfssl -decrypt aes-ctr-192 -in enc_aes_ctr_192.txt -out dec_aes_ctr_192.txt -pwd hello192
-
-wolfssl -encrypt aes-ctr-256 -pwd hello256 -in ../somejunk/somejunk_100000.txt -out enc_aes_ctr_256.txt
-wolfssl -decrypt aes-ctr-256 -in enc_aes_ctr_256.txt -out dec_aes_ctr_256.txt -pwd hello256
