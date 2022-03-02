0a5fcda8145f9905feda7e788e230e4b8449c735
gojimmypi <13059545+gojimmypi@users.noreply.github.com>
2022-03-01 21:25:51 -0800

    add missing projects skipped with prior .gitignore

---
+++ tls/VisualGDB-tls/client-tls13/client-tls13.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	char sz[] = "Hello, World!\n";	/* Hover mouse over "sz" while debugging to see its contents */
+	printf("%s", sz);	
+	fflush(stdout); /* <============== Put a breakpoint here */
+	return 0;
+}
