0a2c81ca2a6fe752911dc22f2e0ac25ecb6a88f1
David Garske <david@wolfssl.com>
2022-04-04 13:11:52 -0700

    Improvements to LWIP native client example.

--- lwip/README.md
+++ lwip/README.md
@@ -1,9 +1,10 @@
-Assumes an existing STM project with native LwIP support. wolfSSL needs compiled
-with the macro WOLFSSL_LWIP_NATIVE
+# LWIP Native example
 
-The main function to call in the example code is tls_echoclient_connect();
+Assumes an existing STM project with native LwIP support. wolfSSL needs compiled with the macro `WOLFSSL_LWIP_NATIVE`
 
-Macros in lwipopts.h were also adjusted to better handle TLS packets, which can be as large as 16k. The macros adjusted were:
+The main function to call in the example code is `tls_echoclient_connect();`
+
+Macros in `lwipopts.h` were also adjusted to better handle TLS packets, which can be as large as 16k. The macros adjusted were:
 - MEM_SIZE(16*1024)
 - MEMP_NUM_PBUF (10 -> 20)
 - PBUF_POOL_SIZE (8 -> 9)
@@ -11,14 +12,14 @@ Macros in lwipopts.h were also adjusted to better handle TLS packets, which can 
 - TCP_WND (2*TCP_MSS -> 11*TCP_MSS)
 
 
-
 To start up a server for the example client to connect to use the bundled wolfSSL example
 server on a desktop:
 
+```
 cd wolfssl
 ./configure && make
 ./examples/server/server -b -d -p 49151 -c ./certs/server-ecc.pem -k ./certs/ecc-key.pem -i
+```
 
-If having the DEBUG_PORT macro defined and DEBUG_WOLFSSL then also start up a TCP server for listing for debug messages:
-netcat -l -p 12345
-
+If having the `DEBUG_PORT` macro defined and `DEBUG_WOLFSSL` then also start up a TCP server for listing for debug messages:
+`netcat -l -p 12345`
--- lwip/example-lwip-native-echoclient.c
+++ lwip/example-lwip-native-echoclient.c
@@ -1,6 +1,6 @@
 /* example-lwip-native-echoclient.c
  *
- * Copyright (C) 2006-2020 wolfSSL Inc.
+ * Copyright (C) 2006-2022 wolfSSL Inc.
  *
  * This file is part of wolfSSL. (formerly known as CyaSSL)
  *
@@ -26,176 +26,104 @@
 #include <stdio.h>
 #include <string.h>
 
+#ifndef WOLFSSL_USER_SETTINGS
+#include <wolfssl/options.h>
+#endif
 #include <wolfssl/wolfcrypt/settings.h>
 #include <wolfssl/ssl.h>
 
-#define USE_CERT_BUFFERS_2048
-#define USE_CERT_BUFFERS_256
+#ifndef NO_RSA
+    #define USE_CERT_BUFFERS_2048
+    #define CA_CERTS           ca_cert_der_2048
+    #define CA_CERTS_LEN       sizeof_ca_cert_der_2048
+    #define CLIENT_CERT        client_cert_der_2048
+    #define CLIENT_CERT_LEN    sizeof_client_cert_der_2048
+    #define CLIENT_KEY         client_key_der_2048
+    #define CLIENT_KEY_LEN     sizeof_client_key_der_2048
+#elif defined(HAVE_ECC)
+    #define USE_CERT_BUFFERS_256
+    #define CA_CERTS           ca_ecc_cert_der_256
+    #define CA_CERTS_LEN       sizeof_ca_ecc_cert_der_256
+    #define CLIENT_CERT        cliecc_cert_der_256
+    #define CLIENT_CERT_LEN    sizeof_cliecc_cert_der_256
+    #define CLIENT_KEY         ecc_clikey_der_256
+    #define CLIENT_KEY_LEN     sizeof_ecc_clikey_der_256
+#endif
 #include <wolfssl/certs_test.h>
 #include <wolfcrypt/test/test.h>
 
 static err_t tcp_echoclient_connected(void *arg, struct tcp_pcb *tpcb, err_t err);
-static void shutdown(void);
+static void TLS_shutdown(void);
 static int TLS_connect(void);
 static int TLS_setup(void);
 
 struct tcp_pcb *tls_pcb   = NULL;
 struct tcp_pcb *debug_pcb = NULL;
 
-#ifdef DEBUG_PORT
-    static int debugConnected = 0;
-    static int testWolfcrypt = 0; /* set to 1 to not run wolfCrypt tests */
-#else
-    /* default to "connected" for debugging when not used */
-    static int debugConnected = 1;
-    static int testWolfcrypt = 1;
-#endif
 static int tlsConnected = 0;
 static int tcpConnected = 0;
 static int tlsWaitingForReply = 0;
 
-WOLFSSL_CTX *ctx;
-WOLFSSL *ssl;
-
-struct debugging *logging = NULL;
-struct debugging {
-  struct tcp_pcb *pcb;
-  struct pbuf *p_tx;
-};
-
-#ifdef DEBUG_PORT
-static err_t debug_recv(void *arg, struct tcp_pcb *tpcb, struct pbuf *p, err_t err)
-{
-    /* only sending out debug logs, drop any incoming msgs */
-    tcp_recved(tpcb, p->tot_len);
-    pbuf_free(p);
-    return ERR_OK;
-}
-
-static err_t debug_sent(void *arg, struct tcp_pcb *tpcb, u16_t len)
-{
-    return ERR_OK;
-}
+static WOLFSSL_CTX *ctx = NULL;
+static WOLFSSL *ssl = NULL;
 
 void loggingCb(const int logLevel, const char *const logMessage)
 {
-    if (logging != NULL) {
-        tcp_write(debug_pcb, logMessage, strlen(logMessage), TCP_WRITE_FLAG_COPY);
-        tcp_write(debug_pcb, "\n\r", sizeof("\n\r"), TCP_WRITE_FLAG_COPY);
-        tcp_output(debug_pcb);
-    }
-}
-
-/* runs the wolfcrypt test on startup */
-static void run_test_wolfcrypt(void)
-{
-    if (debugConnected == 1 && testWolfcrypt == 0) {
-        if (wolfcrypt_test(NULL) == 0) {
-            loggingCb(0, "wolfcrypt tests passed");
-        }
-        else {
-            loggingCb(0, "wolfcrypt tests failed");
-        }
-        testWolfcrypt = 1;
-    }
-}
-
-static err_t start_debug(void *arg, struct tcp_pcb *tpcb, err_t err)
-{
-    if (err == ERR_OK) {
-        logging = (struct debugging *)mem_malloc(sizeof(struct debugging));
-        logging->pcb = tpcb;
-        tcp_arg(tpcb, logging);
-        tcp_recv(debug_pcb, debug_recv);
-        tcp_sent(tpcb, debug_sent);
-        loggingCb(0, "wolfSSL debug connection");
-        if (testWolfcrypt == 0) {
-            loggingCb(0, "Running wolfCrypt tests, this could take some time...");
-        }
-        debugConnected = 1;
-    }
-    return ERR_OK;
-}
-
-#else
-void loggingCb(const int logLevel, const char *const logMessage)
-{
+    printf("%s\n", logMessage);
     (void)logLevel;
-    (void)logMessage;
 }
-
-static void run_test_wolfcrypt(void)
-{
-    /* do nothing in this case */
-}
-#endif /* DEBUG_PORT */
-
 
 void tls_echoclient_connect(void)
 {
     ip_addr_t DestIPaddr;
 
-#ifdef DEBUG_PORT
-    if (debugConnected == 0) {
-        debug_pcb = tcp_new();
-        IP4_ADDR( &DestIPaddr, DEST_IP_ADDR0, DEST_IP_ADDR1, DEST_IP_ADDR2, DEST_IP_ADDR3 );
-        tcp_connect(debug_pcb, &DestIPaddr, DEBUG_PORT, start_debug);
+    if (tcpConnected == 0) {
+        tls_pcb = tcp_new();
+        if (tls_pcb != NULL) {
+            IP4_ADDR( &DestIPaddr, DEST_IP_ADDR0, DEST_IP_ADDR1, DEST_IP_ADDR2, DEST_IP_ADDR3 );
+            tcp_connect(tls_pcb,&DestIPaddr,DEST_PORT,tcp_echoclient_connected);
+        }
     }
-#endif
 
-    if (debugConnected == 1 && testWolfcrypt == 1) {
-        if (tcpConnected == 0) {
-            tls_pcb = tcp_new();
-            if (tls_pcb != NULL) {
-                IP4_ADDR( &DestIPaddr, DEST_IP_ADDR0, DEST_IP_ADDR1, DEST_IP_ADDR2, DEST_IP_ADDR3 );
-                tcp_connect(tls_pcb,&DestIPaddr,DEST_PORT,tcp_echoclient_connected);
+    if (tcpConnected == 1 && tlsConnected == 0) {
+        TLS_connect();
+    }
+
+    if (tlsConnected == 1) {
+        int ret;
+        char reply[256];
+        int err;
+
+        if (tlsWaitingForReply == 0) {
+            ret = wolfSSL_write(ssl, "TLS **TEST 1** ", sizeof("TLS **TEST 1** "));
+            if (ret <= 0) {
+                loggingCb(0, "error when writing TLS message");
+                shutdown();
+            }
+            else {
+                tlsWaitingForReply = 1;
             }
         }
 
-        if (tcpConnected == 1 && tlsConnected == 0) {
-            TLS_connect();
-        }
-
-        if (tlsConnected == 1) {
-            int ret;
-            char reply[256];
-            int err;
-
-            if (tlsWaitingForReply == 0) {
-                ret = wolfSSL_write(ssl, "TLS **TEST 1** ", sizeof("TLS **TEST 1** "));
-                if (ret <= 0) {
-                    loggingCb(0, "error when writing TLS message");
+        /* read a reply from the server */
+        if (tlsWaitingForReply == 1) {
+            memset(reply, 0, sizeof(reply));
+            ret = wolfSSL_read(ssl, reply, sizeof(reply));
+            if (ret <= 0) {
+                err = wolfSSL_get_error(ssl, 0);
+                if (err != SSL_ERROR_WANT_READ &&
+                    err != WOLFSSL_ERROR_WANT_WRITE) {
+                    loggingCb(0, "error when reading TLS message");
                     shutdown();
                 }
-                else {
-                    tlsWaitingForReply = 1;
-                }
             }
-
-            /* read a reply from the server */
-            if (tlsWaitingForReply == 1) {
-                memset(reply, 0, sizeof(reply));
-                ret = wolfSSL_read(ssl, reply, sizeof(reply));
-                if (ret <= 0) {
-                    err = wolfSSL_get_error(ssl, 0);
-                    if (err != SSL_ERROR_WANT_READ &&
-                        err != WOLFSSL_ERROR_WANT_WRITE) {
-                        loggingCb(0, "error when reading TLS message");
-                        shutdown();
-                    }
-                }
-                if (ret > 0) {
-                    loggingCb(0, reply);
-                    shutdown(); /* received reply, done with connection */
-                }
+            if (ret > 0) {
+                loggingCb(0, reply);
+                shutdown(); /* received reply, done with connection */
             }
         }
     }
-    else {
-        run_test_wolfcrypt();
-    }
 }
-
 
 static err_t tcp_echoclient_connected(void *arg, struct tcp_pcb *tpcb, err_t err)
 {
@@ -226,11 +154,8 @@ static int TLS_setup(void)
         return ERR_MEM;
     }
 
-    // This is an example of loading an RSA CA certificate instead of ECC
-    //ret = wolfSSL_CTX_load_verify_buffer(ctx, ca_cert_der_2048, sizeof(ca_cert_der_2048), WOLFSSL_FILETYPE_ASN1);
-    //
-
-    ret = wolfSSL_CTX_load_verify_buffer(ctx, ca_ecc_cert_der_256, sizeof(ca_ecc_cert_der_256), WOLFSSL_FILETYPE_ASN1);
+    ret = wolfSSL_CTX_load_verify_buffer(ctx, CA_CERTS, CA_CERTS_LEN,
+        WOLFSSL_FILETYPE_ASN1);
     if (ret != WOLFSSL_SUCCESS) {
         loggingCb(0, "error loading in verify buffer");
         return ERR_MEM;
@@ -266,7 +191,7 @@ static int TLS_connect(void)
 }
 
 /* close all connections and free TLS session */
-static void shutdown(void)
+static void TLS_shutdown(void)
 {
     if (ssl != NULL) {
         wolfSSL_shutdown(ssl);
@@ -274,7 +199,7 @@ static void shutdown(void)
     }
 
     if (ctx != NULL) {
-            wolfSSL_CTX_free(ctx);
+        wolfSSL_CTX_free(ctx);
     }
 
     tlsConnected = 0;
@@ -297,4 +222,3 @@ static void shutdown(void)
         tcpConnected = 0;
     }
 }
-
