cf2c577177815eaeac6e58f208266ffd85390fb8
Anthony Hu <anthony@wolfssl.com>
2022-11-25 09:15:31 -0500

    Update PQM4 kyber setup script.
    Until now there was a bug in the PQM4 library that did not allow us to turn on
    optimizations. That bug has been resolved. So, now we use the current head as
    of today.

--- pq/stm32/setup_kyber.sh
+++ pq/stm32/setup_kyber.sh
@@ -14,15 +14,15 @@ tmp_dir=$PWD
 
 git clone https://github.com/mupq/pqm4.git $tmp_dir/pqm4
 cd $tmp_dir/pqm4
-git checkout 3bfbbfd30401bd1dce3c497feb2a152713f2e735
+git checkout 1eeb74e4106a80e26a9452e4793acd6f191fe413
 
 git clone https://github.com/mupq/mupq.git $tmp_dir/mupq
 cd $tmp_dir/mupq
-git checkout 0235e325b60dcbd41bc82cdee55b5e3940e70b3e
+git checkout 3b48fa5aff6f5921df5b3444450281daca6d21d1
 
 git clone  https://github.com/pqclean/pqclean.git $tmp_dir/pqclean
 cd $tmp_dir/pqclean
-git checkout 964469d5506f3068c5afb504e3406f3291f6c336
+git checkout 2cc6e744e63c9aa7703ba3ae3304215e83bba167
 
 # Populate include directory
 cp $tmp_dir/pqm4/crypto_kem/kyber512/m4fspeed/params.h $inc_dir
@@ -52,12 +52,12 @@ cp $tmp_dir/pqm4/crypto_kem/kyber512/m4fspeed/macros.i $code_dir
 cp $tmp_dir/pqm4/crypto_kem/kyber512/m4fspeed/matacc.c $code_dir
 cp $tmp_dir/pqm4/crypto_kem/kyber512/m4fspeed/matacc.h $code_dir
 cp $tmp_dir/pqm4/crypto_kem/kyber512/m4fspeed/matacc.i $code_dir
-cp $tmp_dir/pqm4/crypto_kem/kyber512/m4fspeed/matacc.S $code_dir
+cp $tmp_dir/pqm4/crypto_kem/kyber512/m4fspeed/matacc_asm.S $code_dir
 cp $tmp_dir/pqm4/crypto_kem/kyber512/m4fspeed/ntt.c $code_dir
 cp $tmp_dir/pqm4/crypto_kem/kyber512/m4fspeed/ntt.h $code_dir
 cp $tmp_dir/pqm4/crypto_kem/kyber512/m4fspeed/poly.c $code_dir
 cp $tmp_dir/pqm4/crypto_kem/kyber512/m4fspeed/poly.h $code_dir
-cp $tmp_dir/pqm4/crypto_kem/kyber512/m4fspeed/poly.S $code_dir
+cp $tmp_dir/pqm4/crypto_kem/kyber512/m4fspeed/poly_asm.S $code_dir
 cp $tmp_dir/pqm4/crypto_kem/kyber512/m4fspeed/polyvec.c $code_dir
 cp $tmp_dir/pqm4/crypto_kem/kyber512/m4fspeed/polyvec.h $code_dir
 cp $tmp_dir/pqm4/crypto_kem/kyber512/m4fspeed/reduce.S $code_dir
@@ -68,16 +68,14 @@ cp $tmp_dir/pqm4/crypto_kem/kyber512/m4fspeed/verify.h $code_dir
 
 # Rename some files
 mv $inc_dir/api.h $inc_dir/api_kyber.h
-mv $code_dir/matacc.S $code_dir/matacc_renamed.S
-mv $code_dir/poly.S $code_dir/poly_renamed.S
 
 # Patch in some small fixups.
 cd $cur_dir
 patch -p0 < setup_kyber.patch
 
 # give the user some instructions
-echo "Created $code_dir . Plese add it to your project's Core directory."
-echo "Created $inc_dir . Plese add the files it contains to you project's Core/Inc directory."
+echo "Created $code_dir . Please add it to your project's Core directory."
+echo "Created $inc_dir . Please add the files it contains to you project's Core/Inc directory."
 
 # Cleanup
 rm -rf $tmp_dir
