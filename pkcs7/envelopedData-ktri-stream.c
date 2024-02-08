7f7170609e7e0057df9df73599ec921be29e5471
JacobBarthelmeh <jacob@wolfssl.com>
2024-02-09 03:43:44 +0700

    add content/stream callback example

--- pkcs7/envelopedData-ktri-stream.c
+++ pkcs7/envelopedData-ktri-stream.c
@@ -1,4 +1,4 @@
-/* envelopedData-ktri.c
+/* envelopedData-ktri-stream.c
  *
  * Copyright (C) 2006-2020 wolfSSL Inc.
  *
@@ -18,6 +18,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
  */
+
 #include <wolfssl/options.h>
 #include <wolfssl/wolfcrypt/settings.h>
 #include <wolfssl/wolfcrypt/pkcs7.h>
@@ -27,12 +28,33 @@
 #define certFile "../certs/client-cert.der"
 #define keyFile  "../certs/client-key.der"
 
-#define encodedFileKTRI "envelopedDataKTRI.der"
+#define encodedFileKTRI "envelopedDataKTRI-stream.der"
 
-static const byte data[] = { /* Hello World */
-    0x48,0x65,0x6c,0x6c,0x6f,0x20,0x57,0x6f,
-    0x72,0x6c,0x64
-};
+FILE *fileOut, *fileIn;
+#define TEST_SIZE 256
+static byte* contentRead = NULL;
+
+static int GetContentCB(PKCS7* pkcs7, byte** content)
+{
+    int ret;
+
+    ret = fread(contentRead, 1, TEST_SIZE, fileIn);
+    *content = contentRead;
+
+    return ret;
+}
+
+
+static int StreamOutputCB(PKCS7* pkcs7, const byte* output, word32 outputSz)
+{
+    if (outputSz > 0) {
+        if (fwrite(output, 1, outputSz, fileOut) != outputSz) {
+            return -1;
+        }
+    }
+    return 0;
+}
+
 
 static int load_certs(byte* cert, word32* certSz, byte* key, word32* keySz)
 {
@@ -57,30 +79,10 @@ static int load_certs(byte* cert, word32* certSz, byte* key, word32* keySz)
     return 0;
 }
 
-static int write_file_buffer(const char* fileName, byte* in, word32 inSz)
-{
-    int ret;
-    FILE* file;
-
-    file = fopen(fileName, "wb");
-    if (file == NULL) {
-        printf("ERROR: opening file for writing: %s\n", fileName);
-        return -1;
-    }
-
-    ret = (int)fwrite(in, 1, inSz, file);
-    if (ret == 0) {
-        printf("ERROR: writing buffer to output file\n");
-        return -1;
-    }
-    fclose(file);
-
-    return 0;
-}
 
 static int envelopedData_encrypt(byte* cert, word32 certSz, byte* key,
                                  word32 keySz, byte* out, word32 outSz,
-                                 byte useStreamMode)
+                                 word32 contentSz, byte useStreamMode)
 {
     int ret;
     PKCS7* pkcs7;
@@ -89,13 +91,13 @@ static int envelopedData_encrypt(byte* cert, word32 certSz, byte* key,
     if (pkcs7 == NULL)
         return -1;
 
-    pkcs7->content        = (byte*)data;
-    pkcs7->contentSz      = sizeof(data);
+    pkcs7->content        = NULL;
+    pkcs7->contentSz      = contentSz;
     pkcs7->contentOID     = DATA;
     pkcs7->encryptOID     = AES256CBCb;
 
     if (useStreamMode) {
-        wc_PKCS7_SetStreamMode(pkcs7, 1, NULL, NULL);
+        wc_PKCS7_SetStreamMode(pkcs7, 1, GetContentCB, StreamOutputCB);
     }
 
     /* add recipient using RSA certificate (KTRI type) */
@@ -116,11 +118,6 @@ static int envelopedData_encrypt(byte* cert, word32 certSz, byte* key,
     } else {
         printf("Successfully encoded EnvelopedData bundle (%s), stream mode"
                " %d\n", encodedFileKTRI, useStreamMode);
-
-        if (write_file_buffer(encodedFileKTRI, out, ret) != 0) {
-            printf("ERROR: error writing encoded to output file\n");
-            return -1;
-        }
     }
 
     wc_PKCS7_Free(pkcs7);
@@ -155,9 +152,9 @@ static int envelopedData_decrypt(byte* in, word32 inSz, byte* cert,
 
     /* decode envelopedData, returns size */
     ret = wc_PKCS7_DecodeEnvelopedData(pkcs7, in, inSz, out, outSz);
-    if (ret <= 0 || (ret != sizeof(data)) || (XMEMCMP(out, data, ret) != 0)) {
-        printf("Failed to decode EnvelopedData bundle (%s)\n",
-                encodedFileKTRI);
+    if (ret <= 0) {
+        printf("Failed to decode EnvelopedData bundle (%s), error %d\n",
+                encodedFileKTRI, ret);
         wc_PKCS7_Free(pkcs7);
         return -1;
     } else {
@@ -170,69 +167,124 @@ static int envelopedData_decrypt(byte* in, word32 inSz, byte* cert,
     return ret;
 }
 
-#ifdef HAVE_PKCS7
+#if defined(HAVE_PKCS7) && defined(ASN_BER_TO_DER)
+
+#define EXTRA_ASN1_SIZE 1024
 
 int main(int argc, char** argv)
 {
-    int ret;
-    int encryptedSz, decryptedSz;
-    word32 certSz, keySz;
+    int ret = 0;
+    int encryptedSz = 0, decryptedSz;
+    word32 certSz, keySz, contentSz;
 
     byte cert[2048];
     byte key[2048];
-    byte encrypted[1024];
-    byte decrypted[1024];
+    byte* encrypted = NULL;
+    byte* decrypted = NULL;
 
 #ifdef DEBUG_WOLFSSL
     wolfSSL_Debugging_ON();
 #endif
 
-    certSz = sizeof(cert);
-    keySz = sizeof(key);
-    ret = load_certs(cert, &certSz, key, &keySz);
-    if (ret != 0)
+    if (argc != 2) {
+        printf("USAGE: ./%s <content file name>\n", argv[0]);
         return -1;
+    }
 
-    encryptedSz = envelopedData_encrypt(cert, certSz, key, keySz,
-                                        encrypted, sizeof(encrypted), 0);
-    if (encryptedSz < 0)
+    if (wolfCrypt_Init() != 0) {
+        printf("Issue with wolfcrypt init\n");
         return -1;
+    }
 
-#ifdef ASN_BER_TO_DER
-    /* recreate the bundle with BER encoding */
-    encryptedSz = envelopedData_encrypt(cert, certSz, key, keySz,
-                                        encrypted, sizeof(encrypted), 1);
-    if (encryptedSz < 0)
+    fileIn  = fopen(argv[1], "rb");
+    if (fileIn == NULL) {
+        printf("Issue opening file %s\n", argv[1]);
         return -1;
-#endif
+    }
 
-#ifdef DEBUG_WOLFSSL
-    printf("EnvelopedData DER (%d byte):\n", encryptedSz);
-    WOLFSSL_BUFFER(encrypted, encryptedSz);
-#endif
+    fileOut = fopen(encodedFileKTRI, "wb");
+    if (fileOut == NULL) {
+        printf("Issue opening file %s\n", encodedFileKTRI);
+        fclose(fileIn);
+        return -1;
+    }
 
-    decryptedSz = envelopedData_decrypt(encrypted, encryptedSz,
+    contentRead = (byte*)XMALLOC(TEST_SIZE, NULL, DYNAMIC_TYPE_TMP_BUFFER);
+    if (contentRead == NULL) {
+        printf("Unable to malloc content read buffer\n");
+        ret = MEMORY_E;
+    }
+
+    if (ret == 0) {
+        fseek(fileIn, 0, SEEK_END);
+        contentSz = ftell(fileIn);
+        fseek(fileIn, 0, SEEK_SET);
+        printf("contentSz = %d\n", contentSz);
+
+        certSz = sizeof(cert);
+        keySz = sizeof(key);
+        ret = load_certs(cert, &certSz, key, &keySz);
+    }
+
+    if (ret == 0) {
+        encryptedSz = envelopedData_encrypt(cert, certSz, key, keySz,
+                                    encrypted, encryptedSz, contentSz, 1);
+        if (encryptedSz < 0) {
+            ret = encryptedSz;
+            printf("Issue %d with encrypt\n", ret);
+        }
+    }
+    fclose(fileIn);
+    fclose(fileOut);
+
+
+#if 1
+    decryptedSz = encryptedSz = contentSz + EXTRA_ASN1_SIZE;
+    if (ret == 0) {
+        encrypted = (byte*)XMALLOC(encryptedSz, NULL, DYNAMIC_TYPE_TMP_BUFFER);
+        decrypted = (byte*)XMALLOC(decryptedSz, NULL, DYNAMIC_TYPE_TMP_BUFFER);
+        if (encrypted == NULL || decrypted == NULL) {
+            ret = MEMORY_E;
+        }
+    }
+
+    if (ret == 0) {
+        FILE* f = fopen(encodedFileKTRI, "rb");
+        encryptedSz = fread(encrypted, 1, encryptedSz, f);
+        fclose(f);
+        if (encryptedSz <= 0) {
+            printf("error reading file %s\n", encodedFileKTRI);
+            ret = -1;
+        }
+    }
+
+    if (ret == 0) {
+        decryptedSz = envelopedData_decrypt(encrypted, encryptedSz,
                                         cert, certSz, key, keySz,
-                                        decrypted, sizeof(decrypted));
-    if (decryptedSz < 0)
-        return -1;
+                                        decrypted, decryptedSz);
+        if (decryptedSz < 0) {
+            ret = decryptedSz;
+            printf("Issue %d with decrypt\n", ret);
+        }
+    }
 
-#ifdef DEBUG_WOLFSSL
-    printf("Decrypted content (%d byte):\n", decryptedSz);
-    WOLFSSL_BUFFER(decrypted, decryptedSz);
 #endif
 
-    (void)argc;
-    (void)argv;
+    if (contentRead != NULL)
+        XFREE(contentRead, NULL, DYNAMIC_TYPE_TMP_BUFFER);
+    if (encrypted != NULL)
+        XFREE(encrypted, NULL, DYNAMIC_TYPE_TMP_BUFFER);
+    if (decrypted != NULL)
+        XFREE(decrypted, NULL, DYNAMIC_TYPE_TMP_BUFFER);
 
-    return 0;
+    return ret;
 }
 
 #else
 
 int main(int argc, char** argv)
 {
-    printf("Must build wolfSSL using ./configure --enable-pkcs7\n");
+    printf("Must build wolfSSL using ./configure --enable-pkcs7 --enable-indef\n");
     return 0;
 }
 
