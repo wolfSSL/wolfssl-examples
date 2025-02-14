81c7e54076429cc1d934e931f9709acf762338ab
Reda Chouk <reda@wolfssl.com>
2025-02-14 11:46:42 +0100

    Added OS-level timeout support for the DTLS I/O callbacks examples

--- dtls/client-dtls-callback.c
+++ dtls/client-dtls-callback.c
@@ -67,8 +67,34 @@ int my_IORecv(WOLFSSL* ssl, char* buff, int sz, void* ctx)
     int recvd;
     struct sockaddr addr;
     socklen_t addrSz = sizeof(addr);
+    int dtls_timeout;
 
     printf("my_IORecv fd %d, buf %d\n", shared->sd, sz);
+
+    /* Get current DTLS handshake timeout. */
+    dtls_timeout = wolfSSL_dtls_get_current_timeout(ssl);
+    printf("my_IORecv timeout %d\n", dtls_timeout);
+
+    /* If we are performing the DTLS handshake, and we are in blocking mode, we
+     * set a socket timeout at OS level. */
+    if (!wolfSSL_is_init_finished(ssl)) {
+            /* Still in handshake: set the OS timeout */
+            if (dtls_timeout > 0) {
+                    struct timeval tv;
+                    tv.tv_sec = dtls_timeout;
+                    tv.tv_usec = 0;
+                    if (setsockopt(shared->sd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0) {
+                            fprintf(stderr, "setsockopt failed\n");
+                            return WOLFSSL_CBIO_ERR_GENERAL;
+                    }
+            }
+    } else {
+            struct timeval tv = {0, 0};
+            if (setsockopt(shared->sd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0) {
+                    fprintf(stderr, "setsockopt failed\n");
+                    return WOLFSSL_CBIO_ERR_GENERAL;
+            }
+    }
 
     /* Receive datagram */
     recvd = recvfrom(shared->sd, buff, sz, 0, &addr, &addrSz);
